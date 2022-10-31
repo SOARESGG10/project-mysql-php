75fc1124d9e64df279c6bae0a864fec3000f6f3f
Ruan Henrique <110910466+RuanHenrique2003@users.noreply.github.com>
2022-10-31 10:58:27 -0300

    Delete agenda.sql

--- agenda.sql
+++
@@ -1,9 +0,0 @@
-use agenda;
-create table  tb_pessoa (
-id int unsigned primary key auto_increment,
-nome varchar (255) not null,
-sexo varchar (1) not null comment 'sexo: M, F, N',
-dt_nacimento date not null comment 'Data de Nascimento',
-telefone decimal (13,0) not null comment 'telefone com DDD e codigo do pais (apenas digitos)',
-email varchar (255) not null comment 'email'
-);

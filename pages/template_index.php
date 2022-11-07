f3d2771d009e38eec6330aa728be6cf6a861ef09
Rafael Henrique <100523456+SOARESGG10@users.noreply.github.com>
2022-11-07 10:54:10 -0300

    Atualizando o template do index.php

--- pages/template_index.php
+++ pages/template_index.php
@@ -5,30 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../assets/css/login.css">
-    <title>Projeto - Agenda</title>
+    <link rel="stylesheet" href="../assets/css/home.css">
+    <title>Document</title>
 </head>
 
 <body>
-            <form action="" class="form" method="POST">
-                <div class="div-titulo">
-                    <h2 class="titulo">Acessar o painel</h2>
-                </div>
-                <div class="inputs">
-                    <div class="input">
-                        <label for="email">Email</label>
-                        <input type="email" name="email" id="email" placeholder="Informe seu email">
-                    </div>
-                    <div class="input">
-                        <label for="senha">Senha</label>
-                        <input type="password" name="senha" id="senha" placeholder="Informe sua senha">
-                    </div>
-                    <div class="input">
-                        <input type="submit" class="botao" name="botao_acessar" value="Entrar">
-                    </div>
-                </div>
-            </form>
-        </div>
+
+    <div class="container"></div>
 </body>
 
 </html>

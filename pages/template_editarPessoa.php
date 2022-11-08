c5427bf731bebbaf01ffc134ef5fd278ad8cd2be
GustavojSaraiva <102226082+GustavojSaraiva@users.noreply.github.com>
2022-11-08 00:21:57 -0300

    Update template_editarPessoa.php

--- pages/template_editarPessoa.php
+++ pages/template_editarPessoa.php
@@ -1,3 +1,13 @@
+<?php
+include "conectabd.php";
+include "funcoes.php";
+
+if ($usuario=verificaUsuarioLogado()){
+    
+} else {
+    header('Location: template_login.php');   
+}
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 
@@ -11,6 +21,18 @@
 </head>
 
 <body>
+    <h1>Agenda - Alterar Pessoa</h1>
+    <?php
+
+    if (!isset($_GET["bt_sub"])) {
+        // primeira vez, então exibe formulário
+
+        # recupera o id passado pela página principal
+        $id_pessoa = $_GET["id"];
+        # usa a função lerPessoa() para recuperar os demais dados correspondentes ao id_pessoa recebido
+        # $pessoa é um vetor cujos índices são os nomes das colunas da tabela tb_pessoa
+        $pessoa = lerPessoa($pdo, $id_pessoa);
+    ?>
                 <div class="div-titulo">
                     <h2 class="titulo">Inserir Pessoa</h2>
                 </div>
@@ -57,6 +79,39 @@
                 </div>
             </form>
         </div>
+<?php
+
+} else {
+  $id_pessoa = $_GET["id"];
+  $ds_nome = $_GET["nome"];
+  $cd_sexo = $_GET["sexo"];
+  $dt_nasc = $_GET["data_nascimento"]; # ano - mes - dia
+  $nr_telefone = $_GET["telefone"];
+  $ds_email = $_GET["email"];
+
+  try {
+    
+      $sql = 'UPDATE agenda.pessoa SET `ds_nome`=:nome, `cd_sexo`=:sexo, `data_nasc`=:data_nascimento, `nr_telefone`=:telefone, `ds_email`=:email WHERE `id_pessoa` = :id  ;';
+      //echo ">>>sql ".$sql. "  fim <<<<";
+      $stmt = $pdo->prepare($sql);
+      $stmt->bindParam(':nome', $ds_nome);
+      //echo '>>> >>>>'.$cd_sexo;
+      $stmt->bindParam(':sexo', $cd_sexo);
+      $stmt->bindParam(':data_nascimento', $dt_nasc);
+      $stmt->bindParam(':telefone', $nr_telefone);
+      $stmt->bindParam(':email', $ds_email);
+      $stmt->bindParam(':id', $id_pessoa);
+      $stmt->execute();
+    
+      if ($stmt->rowCount()>0) {
+        echo "<p>Registro inserido com sucesso</p>"; 
+      }
+    } catch(PDOException $e) {
+      echo 'Erro: ' . $e->getMessage();
+    }
+}
+
+?>
 </body>
 
 </html>

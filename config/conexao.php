aabae6f3ca9cb31df844a10036766165ebc188f3
GustavojSaraiva <102226082+GustavojSaraiva@users.noreply.github.com>
2022-11-07 21:10:30 -0300

    Update conexao.php

--- config/conexao.php
+++ config/conexao.php
@@ -6,10 +6,8 @@ $username = 'aplicacao_agenda';
 $password = 'agenda123';
 
 try {
-    $conexao = new PDO("mysq:host=$host;dbname=$database;", $username, $password);
+    $conexao = new PDO("mysql:host=$host;dbname=$database;", $username, $password);
     $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $erro) {
     echo "Erro: " . $erro->getMessage();
-} catch (Exception $erro) {
-    echo "Erro: " . $erro->getMessage();
-}
+} 
